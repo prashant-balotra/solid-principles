9802b4d7fbdabc763120608cac95f05e06472f0a
prashant <prashant.balotra@tothenew.com>
2025-09-07 18:12:08 +0530

    solid principles

---
+++ src/test/java/com/codewithmonks/solid/SolidPrinciplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithmonks.solid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidPrinciplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
